78f17fc25b83041a407b5d5e42e0d590f283afd0
zzahid93 <zohaibzahid1@gmail.com>
2019-12-11 14:40:35 +0000

    first commit

---
+++ src/main/java/Domain/User.java
@@ -0,0 +1,31 @@
+package Domain;
+
+public class User {
+	
+	private Long id;
+	private String origin;
+	private String destination;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	
+	
+
+}
