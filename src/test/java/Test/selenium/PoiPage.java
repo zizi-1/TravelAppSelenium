1989b7bc1634d5ce8e35023d61dbdbf52ef401eb
ZohaibZahid <zohaibzahid1@gmail.com>
2020-02-06 10:32:22 +0000

    first commit

---
+++ src/test/java/Test/selenium/PoiPage.java
@@ -0,0 +1,11 @@
+package Test.selenium;
+
+public class Poi {
+
+    @Findby(xpath="/html/body/header/div/nav/ul/li[4]/a")
+    private WebElement navbar
+
+    public void selectPoiPage() {
+        result.click();
+    }
+}
---
+++ src/test/java/Test/selenium/addPoi.java
@@ -0,0 +1,4 @@
+package Test.selenium;
+
+public class addPoi {
+}
